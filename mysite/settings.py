de27db0dd0b55e351f1157f8093ed22e33e2d62b
Samuel Aronda <arondasamuel123@gmail.com>
2020-08-31 13:56:10 +0300

    Setup test db configuration

--- mysite/settings.py
+++ mysite/settings.py
@@ -73,17 +73,24 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DATABASE_NAME'),
-        'USER': config('DATABASE_USER'),
-        'PASSWORD': config('DATABASE_PASSWORD')
+if config('MODE') == 'development':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': config('DATABASE_NAME'),
+            'USER': config('DATABASE_USER'),
+            'PASSWORD': config('DATABASE_PASSWORD')
+        }
     }
-}
-
-
+elif config('MODE') == 'test':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': config('TEST_DATABASE_NAME'),
+            'USER': config('TEST_DATABASE_USER'),
+            'PASSWORD': config('TEST_DATABASE_PASSWORD')
+        }
+    }
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
